62af4c3a813e0aa354031e04d255e3d81ea7a293
yufz <yufz_cumt@126.com>
2019-03-24 22:26:33 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/yufz/bydweather/db/County.java
@@ -0,0 +1,46 @@
+package com.yufz.bydweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;
+    //县名
+    private String countyName;
+    //天气id
+    private String weatherId;
+    //所属市id
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
